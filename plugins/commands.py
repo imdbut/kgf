0a32cbc55ed95dfb6951642b76310b3d73a3c78c
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-02-16 22:34:15 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,8 +39,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ 𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩 ⚚', url='https://t.me/RedFoxx_Bot?startgroup=true')
             ],[
-            InlineKeyboardButton('𝐒𝐞𝐚𝐫𝐜𝐡', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('𝐂𝐡𝐚𝐧𝐧𝐞𝐥', url='https://t.me/+veUIdIW2CQ5mOGU5')
+            InlineKeyboardButton('movie', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('𝐂𝐡jkh', url='https://t.me/+veUIdIW2CQ5mOGU5')
             ],[
             InlineKeyboardButton('𝐇𝐞𝐥𝐩', callback_data='help'),
             InlineKeyboardButton('𝐀𝐛𝐨𝐮𝐭', callback_data='about')
